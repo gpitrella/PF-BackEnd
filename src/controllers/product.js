7a7945f285ce4b52c9f9e498ad7595591ea50d8e
dev-back55 <ahabitu@gmail.com>
2022-06-21 10:45:17 -0300

    GetById - GetByName

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -66,7 +66,87 @@ async function createProduct(name,price,discount,stock,description,category,manu
     }
 }
 
+async function getByName(name){
+
+    
+        if(name){
+            let productInDb = await Product.findOne({
+                where: {name:name},
+                include:[{
+                    model:Categories,
+                    attributes: ['name'],
+                    through:{
+                        attributes:[],
+                    }
+                },{
+                    model:Manufacturer,
+                    attributes: ['name'],
+                    through:{
+                        attributes:[],
+                    }
+                }]
+            })
+            console.log(productInDb)
+            let product = []
+            product.push(productInDb)
+            if(!productInDb) throw new Error('the product does not exist')
+            product= product.map(m=>{
+                return {
+                ...m.dataValues, 
+                categories: m.categories?.map(m=>m.name),
+                manufacturers: m.manufacturers?.map(m=>m.name)
+            }})
+            console.log(product)
+            return product
+            }           
+            else{
+                throw new Error('you must provide a product name')
+            }
+       
+    
+}
+
+async function getById(id){
+
+
+        if(id){
+            let productInDb = await Product.findByPk(id, {
+                
+                include:[{
+                    model:Categories,
+                    attributes: ['name'],
+                    through:{
+                        attributes:[],
+                    }
+                },{
+                    model:Manufacturer,
+                    attributes: ['name'],
+                    through:{
+                        attributes:[],
+                    }
+                }]
+            })
+            
+            let product = []
+            product.push(productInDb)
+            product = product.map(m=>{
+                return {
+                ...m.dataValues, 
+                categories: m.categories?.map(m=>m.name),
+                manufacturers: m.manufacturers?.map(m=>m.name)
+            }})
+             
+            return product
+            }           
+            else{
+                throw new Error('you must provide a product id')
+            }
+       
+}
+
 module.exports = {
     createProduct,
-    getAllProduct
+    getAllProduct,
+    getByName,
+    getById
 }
