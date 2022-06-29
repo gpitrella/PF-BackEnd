e94c1ac97afd67a886ad0c872801bcda299332a8
gpitrella <gabriel98_@hotmail.com>
2022-06-29 12:33:30 -0300

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Listening at port:${PORT} ...`); // eslint-disable-line no-console
   });
