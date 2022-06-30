a88ed57c348b98efc4119bf1aa302bc24465df43
battaglialucas <lucassebastianbattaglia@gmail.com>
2022-06-30 18:22:53 -0300

    subo mas verificaciones de categories y manufacturer

--- src/controllers/manufacturer.js
+++ src/controllers/manufacturer.js
@@ -1,28 +1,51 @@
 const { Manufacturer } = require('../db')
 
+const excludeTimeStamps = {attributes: {exclude: ['updatedAt','createdAt']}}
+
+function verifyManufacturerName(name){
+    if (!name) throw new Error("a name is required for the manufacturer");
+    return name.toUpperCase();
+  }
+
+  async function verifyDuplicateManufacturer(name) {
+    let findInDb = await Manufacturer.findOne({ where: { name } });
+    if (findInDb) throw new Error(`the manufacturer ${findInDb.name}  already exists`);
+  }
+
+  async function verifyManufacturerId(id) {
+    if (!id) throw new Error("you must provide a manufacturer id");
+    if(!/^[0-9]*$/.test(id)) throw new Error("the id must be a number");
+  
+    let manufacturerInDb = await Manufacturer.findByPk(id, excludeTimeStamps);
+  
+    if (!manufacturerInDb) throw new Error("the id does not correspond to an existing manufacturer");
+  
+    return manufacturerInDb;
+  }
+
 async function getAllManufacturer(){
-    let manufacturer = await Manufacturer.findAll({attributes: ['name','image']})
+    let manufacturer = await Manufacturer.findAll(excludeTimeStamps)
     return manufacturer
 }
 
 async function createManufacturer(name, image){
-    if(!name) throw new Error('must enter a name')
-    name = name.toUpperCase()
-    
-    let findInDb = await Manufacturer.findOne({where:{name:name}})
-    if(findInDb) throw new Error(`the manufacturer ${findInDb.name}  already exists`)
+    name = verifyManufacturerName(name)
+    await verifyDuplicateManufacturer(name)
 
-    let newManufacturer = await Manufacturer.create({name:name, image: image})
+    let newManufacturer = await Manufacturer.create({ name, image })
     return `manufacturer ${newManufacturer.name} created successfully` 
 }
 
 async function deleteManufacturer(id){
+    await verifyManufacturerId(id)
     await Manufacturer.destroy({where:{id}})
     return 'The manufacturer was remove'
 }
 
 async function updateManufacturer(id, name, image){
-    await Manufacturer.update({name:name, image:image},{where:{id:id}})
+    name = verifyManufacturerName(name)
+    verifyManufacturerId(id)
+    await Manufacturer.update({name, image},{where:{id}})
     return 'the Manufacturer was successfully updated'
 }
 
