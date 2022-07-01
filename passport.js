2039d5d7fc13399ca1fade0587cd9b385133b434
dev-back55 <ahabitu@gmail.com>
2022-07-01 17:13:03 -0300

    passport y app x claves

--- passport.js
+++ passport.js
@@ -1,16 +1,12 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 
-//? cambio de las claves de google para login
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = proceess.env.GOOGLE_CLIENT_SECRET;
-
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
