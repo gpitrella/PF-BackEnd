4126f3383d723286a6c00358e7e7bcf692be532f
battaglialucas <lucassebastianbattaglia@gmail.com>
2022-07-01 11:18:29 -0300

    nuevas verificaciones para controller de product y review

--- src/controllers/review.js
+++ src/controllers/review.js
@@ -1,14 +1,13 @@
 const { Product, Review } = require("../db");
+const { verifyProductId } = require("./product.js");
 
 async function createReview(comment, score, id) {
-    if (!id) throw new Error("you must provide a product id");
+    let productInDb = await verifyProductId(id)
     if (!comment) throw new Error("you must provide a comment");
     if (!score) throw new Error("you must provide a score");
+
     score = Number.parseInt(score)
     if(!( 1 <= score <= 5)) throw new Error("you must provide a valid score");
-
-    let productInDb = await Product.findByPk(Number.parseInt(id));
-    if (!productInDb) throw new Error("the product does not exist");
 
     let newReview = await Review.create({comment :comment.toUpperCase(), score: score});
     await productInDb.addReview(newReview);
