014fda0c9c90cf52167811080e3d2ddd5e228230
Enzo <enz997.ing.ind@gmail.com>
2022-07-08 11:22:27 -0300

    payment and orders

--- src/controllers/purchase_order.js
+++ src/controllers/purchase_order.js
@@ -1,19 +1,19 @@
 const { Product, User, Purchase_order, Product_order, Useraddress, Branch_office } = require("../db");
 
-async function postPurchase_order({ idProduct, idUser,total, description, idMP, status, idAddress, branchOfficeId, items}){
-    let newOrder = await Purchase_order.create({ total, status, idMP, description, items})
-    let findProduct = await Product.findAll({where:{id:idProduct}})
-    let findUser= await User.findAll({where:{id:idUser}})
-    let findAddress= await Useraddress.findAll({where:{id:idAddress}})
-    let findSucursal = await Branch_office.findAll({where:{id:branchOfficeId}})
+// async function postPurchase_order({ idProduct, idUser,total, description, idMP, status, idAddress, branchOfficeId, items}){
+//     let newOrder = await Purchase_order.create({ total, status, idMP, description, items})
+//     let findProduct = await Product.findAll({where:{id:idProduct}})
+//     let findUser= await User.findAll({where:{id:idUser}})
+//     let findAddress= await Useraddress.findAll({where:{id:idAddress}})
+//     let findSucursal = await Branch_office.findAll({where:{id:branchOfficeId}})
 
-    newOrder.addProduct(findProduct)
-    newOrder.addUser(findUser)
-    //newOrder.addUseraddress(findAddress)
-    //newOrder.addBranch_office(findSucursal)
+//     newOrder.addProduct(findProduct)
+//     newOrder.addUser(findUser)
+//     //newOrder.addUseraddress(findAddress)
+//     //newOrder.addBranch_office(findSucursal)
 
-    return newOrder
-}
+//     return newOrder
+// }
 
 async function updateStatus(id,status){
     let update = await Purchase_order.update({status},{where:{id}})
@@ -47,7 +47,7 @@ async function getAllOrders(){
         //     through: {
         //     attributes: [],
         // },
-        // }
+        //}
     ]})
     return orders
 }
@@ -65,7 +65,7 @@ async function usersOrders(id){
 }
 
 module.exports={
-    postPurchase_order,
+    // postPurchase_order,
     getAllOrders,
     updateStatus,
     usersOrders
