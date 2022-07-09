eede5b0d3f5c5c8a04cc5094e77c5e23550af2d1
battaglialucas <lucassebastianbattaglia@gmail.com>
2022-07-09 11:41:00 -0300

    recuperacion y actualizacion de contraseña terminadas, falta mandar el mail

--- src/controllers/password.js
+++ src/controllers/password.js
@@ -6,8 +6,10 @@ const jwt = require('jsonwebtoken');
 async function recoverPassword(email){
     let userInDb = await User.findOne({where:{email}})
     if(!userInDb)throw new Error("that user does not exist");
-    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1lIjoibHVjYXMiLCJwaG90byI6bnVsbCwicGhvbmVfbnVtYmVyIjpudWxsLCJlbWFpbCI6Imx1Y2FzQGdtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwicGFzc3dvcmQiOiIkMmIkMTAkWllTQUFOZWE3ei4zYVNVcFdNZXUwdUpWdTJXWllyR1d3ZVdxQnl1QW95TGZZRExjdW80WE8iLCJpc2FjdGl2ZSI6dHJ1ZSwiaW1hZ2UiOm51bGwsImNyZWF0ZWRBdCI6IjIwMjItMDctMDhUMTk6MzY6NTUuMjEwWiIsInVwZGF0ZWRBdCI6IjIwMjItMDctMDhUMjA6MjI6MDIuNTkwWiJ9LCJpYXQiOjE2NTczMTQ1OTgsImV4cCI6MTY1NzQwMDk5OH0.o0Iyof5DNUYGGLgFTFyN4YKP4jcafr2bHXKWKLDNz3I";
-    jwt.verify(token, authConfig.secret, (err, decoded) => {console.log(decoded)})
+
+    let token = jwt.sign({ user: userInDb }, authConfig.secret, {expiresIn: authConfig.expires});
+    console.log(token)
+
     // mandar mail bonito para que recupere su contra
     return "We have sent an email to your mailbox so that you can update your password"
 }
@@ -16,11 +18,15 @@ async function updatePassword(password, token){
     if(!(5 < password.length)) throw new Error("the password must have more than 5 characters");
     if(!(password.length < 256)) throw new Error("the password is too long");
 
-    let userInDb = await User.findOne({where:{password : token}})
-    if(!userInDb)throw new Error("that user does not exist");
+    let userInDb;
+
+    jwt.verify(token, authConfig.secret, (error, decoded) => {
+        if(error) throw new Error('That user does not exist')
+        userInDb = decoded.user
+    })
 
     password = bcrypt.hashSync(password, Number.parseInt(authConfig.rounds));
-    await User.update({password},{where:{id : userInDb.dataValues.id}})
+    await User.update({password},{where:{id : userInDb.id}})
 
     return ("the password has been updated successfully")
 }
